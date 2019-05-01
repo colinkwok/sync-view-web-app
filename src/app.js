3c733bff60b7f90a73612f4d6e5038591b02a54b
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-04-30 19:26:44 -0700

    Set up app for Heroku

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require('express')
 const jquery = require('jquery')
 // express is a function called to create a new express application
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname,'../public' )
 const viewsPath = path.join(__dirname, '../templates/views')
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -20,9 +21,8 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
-const port = 3000
 
 // starting the server asynchronously, callback function indicates when server is up
 app.listen(port, () => {
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port ' + port)
 })
