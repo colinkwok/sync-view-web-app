0a69b4de8fa9cea846ceab5853e90ebc8cf035cb
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-05-03 00:46:05 -0700

    Added Synchronous feature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const express = require('express')
 const jquery = require('jquery')
+
 // express is a function called to create a new express application
 const app = express()
+
+var http = require('http').Server(app)
+var io = require('socket.io')(http);
 const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname,'../public' )
@@ -21,8 +25,52 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
+io.on('connection', function(socket) {
+    console.log('an user connected');
+    //io.emit('Welcome')
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('playEvent', function(msg) {
+        console.log('Sending event from server to client')
+        console.log(msg.state)
+        io.emit('play', msg)
+    })
+    socket.on('pauseEvent', function(msg) {
+        console.log('Sending event from server to client')
+        console.log(msg.state)
+        io.emit('pause', msg)
+    })
+    socket.on('seekEvent', function(msg) {
+        console.log('Sending event from server to client')
+        console.log(msg.state)
+        io.emit('seek', msg)
+    })
+    
+    socket.on('hello to server', function() {
+        console.log('recieved hello from client')
+        io.emit('hello to client')
+    })
+
+    // socket.on('play', function(msg) {
+    //     io.emit(msg) 
+    //     //player.playVideo();
+    //     console.log("server: "+ msg)
+    // });
+    // socket.on('pause', function(msg) {
+    //     io.emit(msg)
+    //     //player.pauseVideo();
+    //     console.log("server: "+ msg)
+    // });
+    // socket.on('seek', function(msg) {
+    //     io.emit(msg)
+    //     //player.seekTo(msg, true);
+    //     console.log("server: "+ msg)
+    // });
+})
+
 
 // starting the server asynchronously, callback function indicates when server is up
-app.listen(port, () => {
+http.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
